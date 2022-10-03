7fbd0cb4a22999755bada9148e2a8d948d228b55
Saikiran Patil <patilrsaikiran@gmail.com>
2022-10-03 18:34:24 +0530

    added dotenv and initial databse set-up

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
 
-const port = 3000;
+const dotenv = require("dotenv");
+const bodyparser = require("body-parser");
 
-app.get('/', function (req, res) {
-    res.write("Hello World");
-    res.send();
-})
+// path for config 
+dotenv.config({ path: "./config.env" });
+const port = process.env.PORT;
+
+
+// import db    
+const connectDB = require("./server/database/connections");
+// connecting to database 
+connectDB();
+
+app.use(express.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+
+// import routes
+const userRoutes = require('./server/routes/userRoutes');
+app.use('/',userRoutes);
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
