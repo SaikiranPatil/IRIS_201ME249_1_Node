949387e9e9e255ad3449ed99e5435f544481a89a
Saikiran Patil <patilrsaikiran@gmail.com>
2022-10-13 19:30:33 +0530

    toast to display messages on screen

---
+++ frontend/src/Components/Utils/toast.js
@@ -0,0 +1,74 @@
+import { toast } from 'react-toastify';
+
+export const display = (message, type) => {
+    setTimeout(() => {
+
+        switch (type) {
+            case "info":
+                toast.info(message, {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+                break;
+
+            case "sucess":
+                toast.success(message, {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+                break;
+
+            case "warn":
+                toast.warn(message, {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+                break;
+
+            case "error":
+                toast.error(message, {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+                break;
+
+            default:
+                toast(message, {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+                break;
+        }
+    }, 500);
+
+}
