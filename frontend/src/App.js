c2ef569eaed83f61b89caac0e0725cbfa410df10
Saikiran Patil <patilrsaikiran@gmail.com>
2022-10-13 19:34:14 +0530

    added route for forgot , all tasks, profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,36 @@
 import './App.css';
+import React from "react";
+import store from "./redux/store";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { loadUser } from "./redux/actions/userAction";
 import Header from './Components/Layouts/Header/Header'
 import Footer from './Components/Layouts/Footer/Footer'
 import Login from './Components/User/Login/Login'
 import Register from './Components/User/Register/Register'
+import { ToastContainer } from 'react-toastify';
+import Profile from './Components/User/Profile/Profile';
+import Forgot from './Components/User/ForgotPassword/ForgotPassword';
+import AllTasks from './Components/Tasks/AllTasks/AllTasks';
 
 function App() {
+  const { isAuthenticated, user } = useSelector(state => state.user);
+
+  React.useEffect(() => {
+    store.dispatch(loadUser());
+  }, [])
   return (
     <BrowserRouter>
-      <Header />
+      <Header user={user} isAuthenticated={isAuthenticated} />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/forgot" element={<Forgot />} />
+        <Route path="/account" element={<Profile user={user} isAuthenticated={isAuthenticated} />} />
+        <Route path="/tasks" element={<AllTasks />} />
       </Routes>
       <Footer />
+      <ToastContainer />
     </BrowserRouter>
   );
 }
